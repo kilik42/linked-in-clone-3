f7a002d723663afe1119069bdc688bd9adc3146b
kilik42 <marvinevins@yahoo.com>
2023-11-09 05:09:26 -0600

    initial commit

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 function Header() {
   return (
     <div className='header'>
@@ -19,7 +25,11 @@ function Header() {
 
         <div className="header__right">
            
-
+              <HeaderOption Icon={HomeIcon} title="home"/>
+              <HeaderOption Icon ={SupervisorAccountIcon}title ='My Network'/>
+              <HeaderOption Icon ={BusinessCenterIcon} title="Jobs"/>
+              <HeaderOption Icon ={ChatIcon} title="Messaging"/>
+              <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
             </div>
     </div>
   )
