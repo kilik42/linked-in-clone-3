723089357e3d80e416fb13859d34dbe2fcb66b47
kilik42 <marvinevins@yahoo.com>
2023-12-12 20:09:06 -0600

    working on the form functionality

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth } from './firebase'
 import "./Login.css"
 function Login() {
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
 
   const register = () => {
     console.log("register");
-
+    if (!name) {
+      return alert("please enter a full name");
+    }
 
   } 
 
-  const loginToApp = () => {
+  const loginToApp = (e) => {
     console.log("login");
-      
+    e.preventDefault();
+
   }
 
   return (
@@ -20,10 +29,14 @@ function Login() {
       <h2>please login to continue</h2>
       <form action="">
 
-        <input type="text" placeholder="Full name (required if registering)" />
-        <input type="text" placeholder="Profile pic URL (optional)" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Full name (required if registering)" 
+        value={name} onChange={e => setName(e.target.value)}/>
+        
+        <input type="text" placeholder="Profile pic URL (optional)" 
+        value={profilePic} onChange={e => setProfilePic(e.target.value)}
+        />
+        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
+        <input type="password" placeholder="Password"  value={password} onChange={e => setPassword(e.target.value)}/>
         <button type="submit" onClick={loginToApp}>Sign In</button>
 
 
