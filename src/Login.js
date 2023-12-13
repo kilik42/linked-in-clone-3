d94e91c5ab77c9815145deced4c921c07cb0e9aa
kilik42 <marvinevins@yahoo.com>
2023-12-12 20:01:49 -0600

    working on the login css

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import "./Login.css"
 function Login() {
+
+  const register = () => {
+    console.log("register");
+
+
+  } 
+
+  const loginToApp = () => {
+    console.log("login");
+      
+  }
+
   return (
     <div className = "login">
       <img src="https://download.logo.wine/logo/LinkedIn/LinkedIn-Logo.wine.png" alt="" />
@@ -12,10 +24,14 @@ function Login() {
         <input type="text" placeholder="Profile pic URL (optional)" />
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={loginToApp}>Sign In</button>
 
-        
+
       </form>
+      <p>
+        not a member?
+        <span className="login__register" onClick = {register}>Register Now</span>
+      </p>
     </div>
   )
 }
